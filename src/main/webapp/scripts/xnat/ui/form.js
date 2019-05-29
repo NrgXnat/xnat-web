NrgXnat/xnat-web

commit 2a09667769a7e9a0b308c2777dbfb83d6f2d3c79

Bugfix: form parsing now accounts for missing JSON keys (when db has NULL value)

src/main/webapp/scripts/xnat/ui/form.js
@@ -246,7 +246,10 @@ var XNAT = getObject(XNAT);
             }
 
         });
-        return firstDefined(data,'');
+        if (typeof data === 'object') {
+            return '';
+        }
+        return data;
     }
 
     function setFieldValues(inputs, data){

commit affa3cc79183e668adafa8e4b2d14d5d10f6d85c

Bugfix: commit 2a09667 broke array inputs, this fixes that problem

src/main/webapp/scripts/xnat/ui/form.js
@@ -222,12 +222,14 @@ var XNAT = getObject(XNAT);
         var delimiter = ':'; // See line 645 xhr.js
         var arrayItemRegexp = /\[([0-9]*)\]/;
         var matches, arrNm, idx;
+        var mod = false;
         $.each(name.split(delimiter), function(i, item) {
             matches = arrayItemRegexp.exec(item);
             if (matches === null) {
                 // Not an array
                 if (data.hasOwnProperty(item)) {
                     data = data[item];
+                    mod = true;
                 } else {
                     return ''; // we can't find this key, not sure what to do so just return empty string
                 }
@@ -239,6 +241,7 @@ var XNAT = getObject(XNAT);
                     idx = matches[1];
                     if (idx && Array.isArray(data)) {
                         data = data[idx];
+                        mod = true;
                     }
                 } else {
                     return ''; // we can't find this key, not sure what to do so just return empty string
@@ -246,7 +249,7 @@ var XNAT = getObject(XNAT);
             }
 
         });
-        if (typeof data === 'object') {
+        if (!mod) {
             return '';
         }
         return data;