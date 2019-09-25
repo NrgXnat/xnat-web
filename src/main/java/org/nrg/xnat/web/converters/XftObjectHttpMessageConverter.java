1bbfc3659a147e526ea1c5bb8f58deb130dacc0f
Rick Herrick <jrherrick@wustl.edu>
2019-09-25 09:52:33 -0500

    Integrated custom serializer implementations to handle ItemI-based classes

--- src/main/java/org/nrg/xnat/web/converters/XftObjectHttpMessageConverter.java
+++ src/main/java/org/nrg/xnat/web/converters/XftObjectHttpMessageConverter.java
@@ -1,10 +1,9 @@
 package org.nrg.xnat.web.converters;
 
-import org.apache.commons.io.IOUtils;
+import org.nrg.xdat.XDAT;
+import org.nrg.xdat.base.BaseElement;
 import org.nrg.xft.ItemI;
-import org.nrg.xft.XFTItem;
-import org.nrg.xft.exception.ElementNotFoundException;
-import org.nrg.xft.exception.XFTInitException;
+import org.nrg.xft.schema.Wrappers.XMLWrapper.SAXReader;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
@@ -12,6 +11,8 @@ import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.stereotype.Component;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -35,17 +36,14 @@ public class XftObjectHttpMessageConverter extends AbstractHttpMessageConverter<
     @Override
     protected ItemI readInternal(final Class<? extends ItemI> clazz, final HttpInputMessage message) throws IOException, HttpMessageNotReadableException {
         try (final InputStreamReader input = new InputStreamReader(message.getBody())) {
-            final String value = IOUtils.toString(input);
-            return XFTItem.NewItem(value, null);
-        } catch (ElementNotFoundException e) {
-            throw new HttpMessageNotReadableException("Couldn't create the requested type of object: " + e.ELEMENT, e);
-        } catch (XFTInitException e) {
-            throw new HttpMessageNotReadableException("An error occurred trying to access XFT", e);
+            return BaseElement.GetGeneratedItem(new SAXReader(XDAT.getUserDetails()).parse(new InputSource(input)));
+        } catch (SAXException e) {
+            throw new HttpMessageNotReadableException("An error occurred trying to parse the submitted XML", e);
         }
     }
 
     @Override
-    protected void writeInternal(final ItemI item, final HttpOutputMessage message) throws IOException, HttpMessageNotWritableException {
+    protected void writeInternal(final ItemI item, final HttpOutputMessage message) throws HttpMessageNotWritableException {
         try (final Writer output = new OutputStreamWriter(message.getBody())) {
             item.toXML(output, true);
         } catch (Exception e) {
