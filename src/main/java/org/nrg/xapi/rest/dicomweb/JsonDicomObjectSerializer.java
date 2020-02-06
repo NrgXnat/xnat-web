86d0b5896cd2bc437334c005e9b66ec36a8cdf05
Dave Maffitt <david.maffitt@wustl.edu>
2020-02-06 09:00:30 -0600

    Changes to enable build after rebase on develop:1.7.7-SNAPSHOT

--- src/main/java/org/nrg/xapi/rest/dicomweb/JsonDicomObjectSerializer.java
+++ src/main/java/org/nrg/xapi/rest/dicomweb/JsonDicomObjectSerializer.java
@@ -3,7 +3,7 @@ package org.nrg.xapi.rest.dicomweb;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.github.pgelinas.jackson.javax.json.stream.JacksonGenerator;
+//import com.github.pgelinas.jackson.javax.json.stream.JacksonGenerator;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.json.JSONWriter;
 
@@ -16,10 +16,11 @@ public class JsonDicomObjectSerializer extends StdSerializer<Attributes> {
     @Override
     public void serialize(Attributes value, JsonGenerator gen, SerializerProvider provider) {
 
-        JacksonGenerator jgen = new JacksonGenerator( gen);
-        JSONWriter jsonWriter = new JSONWriter(jgen);
+//        JacksonGenerator jgen = new JacksonGenerator( gen);
+        javax.json.stream.JsonGenerator sgen = (javax.json.stream.JsonGenerator) gen;
+        JSONWriter jsonWriter = new JSONWriter( sgen);
         jsonWriter.write( value);
-        jgen.flush();
+        sgen.flush();
     }
 
 }
