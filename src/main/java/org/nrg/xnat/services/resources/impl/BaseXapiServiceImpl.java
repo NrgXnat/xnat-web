3d55b2da821c3199221b85550411bb03abde3146
Sachin Nirne <sachin.n@A4G381-PC.afourtech.pune>
2020-09-11 15:22:01 +0530

    removed base service interface and added base service Impl

--- src/main/java/org/nrg/xnat/services/resources/impl/BaseXapiServiceImpl.java
+++ src/main/java/org/nrg/xnat/services/resources/impl/BaseXapiServiceImpl.java
@@ -1,28 +1,22 @@
 package org.nrg.xnat.services.resources.impl;
 
-import org.nrg.xnat.services.resources.BaseXapiService;
+public abstract class BaseXapiServiceImpl{
 
-public abstract class BaseXapiServiceImpl implements BaseXapiService{
-
-	@Override
 	public boolean allowPut() {
 		return true;
 		
 	}
 	
-	@Override
 	public boolean allowDelete() {
 		return true;
 		
 	}
 	
-	@Override
 	public boolean allowGet() {
 		return true;
 		
 	}
 	
-	@Override
 	public boolean allowPost() {
 		return true;
 		
