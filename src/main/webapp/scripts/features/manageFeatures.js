07a8117ed9e15baefec517df64a2eed7cd8f61e3
David Maffitt <davidmaffitt@flywheel.io>
2022-08-10 12:35:15 -0500

    Internal-895 Make Feature Management UI more scalable.

--- src/main/webapp/scripts/features/manageFeatures.js
+++ src/main/webapp/scripts/features/manageFeatures.js
@@ -11,92 +11,12 @@ XNAT.app.featureMgr={
 	features:[],
 	groupLoadSuccess:function(resp){
 		//once the existing settings are loaded from the server for these groups, the UI needs to be generated
-		var parsedResponse = YAHOO.lang.JSON.parse(resp.responseText);
-		var _html="";
-		$(parsedResponse).each(function(i1,v1){
-			var proj=parsedResponse[i1];
-			$(proj.groups).each(function(i2,v2){
-    			var group=proj.groups[i2];
-    			_html+='<tr class="featureItem ';
-				if(group.isRole){
-					_html+=' siteRole ';
-				}else{
-					_html+=' highlight ';
-				}
-				_html+='" id="">';
-				
-				if(XNAT.app.featureMgr.level=="group"){
-    				_html+='<td class="featureProject">' + proj.id +'</td>';
-    			}
-				
-				_html+='<td class="featureGroup';
-				if(group.isRole){
-					_html+=' siteRole ';
-				}
-				_html+='">' + group.display +'</td>';
-				
-    			$(XNAT.app.featureMgr.features).each(function(i3,v3){
-    				var feature=XNAT.app.featureMgr.features[i3];
-    				_html+='<td class="';
-    				if(group.isRole){
-    					_html+=' siteRole ';
-    				}
-    				if(!(proj.banned!=undefined && proj.banned.contains(feature.key))){
-    					_html+=' featureEnabled checkbox ';
-    				}
-    				_html+='"><label><input type="checkbox"';
-					_html+=' data-feature="' + feature.key + '" data-group="' + group.id + '" ';
-					
-    				
-					if(proj.banned!=undefined && proj.banned.contains(feature.key)){
-						_html+=' DISABLED  style="display:none"';
-					}else if(XNAT.app.featureMgr.level=="group" && proj.onByDefault!=undefined && proj.onByDefault.contains(feature.key)){
-    					//only used in project level view
-						if(group.inherited_banned!=undefined && group.inherited_banned.contains(feature.key)){
-    						if((group.features!=undefined && group.features.contains(feature.key))) {
-            					_html+=' class="standard featureToggle" CHECKED '; 
-            				}else{
-        						_html+=' class="standard featureToggle"';
-        					}
-    					}else{
-							if((group.blocked!=undefined && group.blocked.contains(feature.key))) {
-            					_html+=' class="inherited inherited-global featureToggle"'; 
-            				}else{
-    							_html+=' class="inherited inherited-global featureToggle half-check"';
-    						}
-						}
-					}else if(XNAT.app.featureMgr.level=="group" && group.inherited_features!=undefined && group.inherited_features.contains(feature.key)){
-						//only used in project level view
-						if((group.blocked!=undefined && group.blocked.contains(feature.key))) {
-        					_html+=' class="inherited inherited-group featureToggle"'; 
-        				}else{
-							_html+=' class="inherited inherited-group featureToggle half-check"';
-						}
-					}else if(XNAT.app.featureMgr.level=="type" && proj.onByDefault!=undefined && proj.onByDefault.contains(feature.key)){
-						//only used in site-wide (group-type) view
-						if((group.blocked!=undefined && group.blocked.contains(feature.key))) {
-        					_html+=' class="inherited inherited-type featureToggle"'; 
-        				}else{
-        					if(group.isRole){
-            					//site roles can't unchecked features that are automatically on
-        						_html+=' class="inherited inherited-type half-check" DISABLED';
-        					}else{
-        						//group types can uncheck them
-        						_html+=' class="inherited inherited-type featureToggle half-check"';
-        					}
-						}
-					}else if((group.features!=undefined && group.features.contains(feature.key))) {
-    					_html+=' class="standard featureToggle" CHECKED '; 
-    				}else{
-						_html+=' class="standard featureToggle"';
-					}
-    				_html+='></label></td>';
-    			});
-    			_html+='</tr>';
-			});
-		});
-		var $featureBody = $("#featureBody");
-        $featureBody.html(_html);
+		let parsedResponse = YAHOO.lang.JSON.parse(resp.responseText);
+
+		let $featureHead = $("#featureHead");
+		$featureHead.html( buildHead( parsedResponse));
+		let $featureBody = $("#featureBody");
+        $featureBody.html( buildBody( parsedResponse));
         $featureBody.closest('.features').fadeIn(100); // show table only after it has the HTML
         $("input.half-check").each(function(i1,v1){
 			//class=inherited implies that this feature was set to OnByDefault at a higher level.  So, it defaults to being on here, but not because it was specifically set here.
@@ -105,7 +25,6 @@ XNAT.app.featureMgr={
 		$("input.inherited.featureToggle").click(function(i1,v1){
 			//had to set this ourselves because IE handles the onchange event weirdly for intermediate boxes.
 			//so we are taking over the marking of enabled/disabled with our own variable to track it.
-			var boxCh=this.checked;
 			$(this).toggleClass("half-check");
 			
 			manageBlocked(this);
@@ -128,6 +47,95 @@ XNAT.app.featureMgr={
 		}
     }
 };
+
+function buildHead( parsedResponse){
+	let proj = parsedResponse[0];
+	let projLabel = (proj.id === '_SITE_WIDE')? 'Site-Wide': proj.id;
+	let html = '<tr><th rowSpan="2">Feature Name</th>';
+	html+= `<th colSpan="${proj.groups.length}">${projLabel}</td></tr><tr>`;
+	$(proj.groups).each(function(iGroup, vGroup){
+		html+= `<th>${proj.groups[iGroup].display}</th>`;
+	});
+	html+= '</tr>';
+	return html;
+}
+
+function buildBody( parsedResponse){
+	let html = '';
+	$(XNAT.app.featureMgr.features).each(function(iFeature,v3){
+		let feature=XNAT.app.featureMgr.features[iFeature];
+		html+='<tr class="featureItem">';
+		html+='<td class="featureProject">' + feature.name +'</td>';
+		$(parsedResponse).each( function(iProj, vProj){
+			let proj = parsedResponse[iProj];
+			$(proj.groups).each( function(iGroup, vGroup) {
+				let group = proj.groups[iGroup];
+				html+= checkbox( feature, proj, group);
+			});
+		});
+		html+='</tr>';
+	});
+	return html;
+}
+
+function checkbox( feature, proj, group){
+	let _html = '';
+	_html+='<td class="';
+	if(group.isRole){
+		_html+=' siteRole ';
+	}
+	if(!(proj.banned!=undefined && proj.banned.contains(feature.key))){
+		_html+=' featureEnabled checkbox ';
+	}
+	_html+='"><label><input type="checkbox"';
+	_html+=' data-feature="' + feature.key + '" data-group="' + group.id + '" ';
+
+	if(proj.banned!=undefined && proj.banned.contains(feature.key)){
+		_html+=' DISABLED  style="display:none"';
+	}else if(XNAT.app.featureMgr.level=="group" && proj.onByDefault!=undefined && proj.onByDefault.contains(feature.key)){
+		//only used in project level view
+		if(group.inherited_banned!=undefined && group.inherited_banned.contains(feature.key)){
+			if((group.features!=undefined && group.features.contains(feature.key))) {
+				_html+=' class="standard featureToggle" CHECKED ';
+			}else{
+				_html+=' class="standard featureToggle"';
+			}
+		}else{
+			if((group.blocked!=undefined && group.blocked.contains(feature.key))) {
+				_html+=' class="inherited inherited-global featureToggle"';
+			}else{
+				_html+=' class="inherited inherited-global featureToggle half-check"';
+			}
+		}
+	}else if(XNAT.app.featureMgr.level=="group" && group.inherited_features!=undefined && group.inherited_features.contains(feature.key)){
+		//only used in project level view
+		if((group.blocked!=undefined && group.blocked.contains(feature.key))) {
+			_html+=' class="inherited inherited-group featureToggle"';
+		}else{
+			_html+=' class="inherited inherited-group featureToggle half-check"';
+		}
+	}else if(XNAT.app.featureMgr.level=="type" && proj.onByDefault!=undefined && proj.onByDefault.contains(feature.key)){
+		//only used in site-wide (group-type) view
+		if((group.blocked!=undefined && group.blocked.contains(feature.key))) {
+			_html+=' class="inherited inherited-type featureToggle"';
+		}else{
+			if(group.isRole){
+				//site roles can't unchecked features that are automatically on
+				_html+=' class="inherited inherited-type half-check" DISABLED';
+			}else{
+				//group types can uncheck them
+				_html+=' class="inherited inherited-type featureToggle half-check"';
+			}
+		}
+	}else if((group.features!=undefined && group.features.contains(feature.key))) {
+		_html+=' class="standard featureToggle" CHECKED ';
+	}else{
+		_html+=' class="standard featureToggle"';
+	}
+	_html+='></label></td>';
+
+	return _html;
+}
 
 function manageEnable(check){
 	//enable/disable this feature/group combo
