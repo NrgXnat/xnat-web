9a4f4445b75aa8a97c9a7f3c3e0b67e1b5b48c2b
Mohana Ramaratnam <mohanaramaratnam@flywheel.io>
2022-12-06 16:56:41 +0000

    LAB-288 Remove multi-page Wizard for MVP
    Approved-by: John Flavin
    Approved-by: Kate Alpert

--- src/main/webapp/scripts/xnat/app/formJson/formJsonConfig_project.js
+++ src/main/webapp/scripts/xnat/app/formJson/formJsonConfig_project.js
@@ -120,7 +120,7 @@ XNAT.plugin =
     }
 
     function initBuilder(form) {
-        let formType = form.data.formType;
+        let formType = "form";
         let formTitle = form.data.formTitle;
         let formBuilderElement = document.getElementById("form-builder");
         let builderConfig = projectFormManager.getBuilderConfiguration();
@@ -510,7 +510,7 @@ XNAT.plugin =
         }else {
             submissionDataObj[PRIMARY_KEY_FIELDNAME] = dbRowId;
         }
-        submissionDataObj['formType'] = configItem['contents']['display'];
+        submissionDataObj['formType'] = "form";
         submissionDataObj['formTitle'] = configItem['contents']['title'];
         let dataXsiType = extractParts(path, 1);
         let dataSingular = XNAT.customFormManager.datatypeManager.getDatatypeByXsiType(dataXsiType).label;
