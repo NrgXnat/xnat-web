84513b64c9da4a82bdb70ba06a6a648cfecaeacf
Tim Olsen <tim@radiologics.com>
2024-02-01 16:51:07 -0600

    Another REST fix

--- src/main/java/org/nrg/xnat/restlet/actions/SessionImporter.java
+++ src/main/java/org/nrg/xnat/restlet/actions/SessionImporter.java
@@ -15,6 +15,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.nrg.action.ActionException;
 import org.nrg.action.ClientException;
 import org.nrg.action.ServerException;
+import org.nrg.dicom.mizer.objects.AnonymizationResult;
+import org.nrg.dicom.mizer.objects.AnonymizationResultNoOp;
 import org.nrg.framework.status.StatusProducer;
 import org.nrg.xdat.XDAT;
 import org.nrg.xdat.om.XnatExperimentdata;
@@ -265,7 +267,8 @@ public class SessionImporter extends ImporterHandlerA implements Callable<List<S
 
                 this.processing("Performing anonymization");
                 final SiteWideAnonymizer   siteWideAnonymizer = new SiteWideAnonymizer(imageSession, true, false);
-                if (!siteWideAnonymizer.call().isEmpty()){
+                List<AnonymizationResult> anonResults = siteWideAnonymizer.call();
+                if (!(anonResults.isEmpty() || anonResults.stream().allMatch(AnonymizationResultNoOp.class::isInstance))){
                     // rebuild XML
                     XnatSubjectdata s = imageSession.getSubjectData();
                     String subject = s != null ? s.getLabel() : imageSession.getSubjectId();
