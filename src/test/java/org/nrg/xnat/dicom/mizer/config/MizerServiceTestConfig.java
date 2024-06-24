3041cd62a0d342dbdd03cdfbfd404363cc06e0cb
David Maffitt <davidmaffitt@flywheel.io>
2024-06-24 12:51:26 -0500

    DE-169 same config change for tests

--- src/test/java/org/nrg/xnat/dicom/mizer/config/MizerServiceTestConfig.java
+++ src/test/java/org/nrg/xnat/dicom/mizer/config/MizerServiceTestConfig.java
@@ -9,8 +9,6 @@
 
 package org.nrg.xnat.dicom.mizer.config;
 
-import org.nrg.dicom.dicomedit.DE6ScriptFactory;
-import org.nrg.dicom.dicomedit.ScriptApplicatorFactory;
 import org.nrg.dicom.mizer.service.MizerServiceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,13 +19,4 @@ import org.springframework.context.annotation.Import;
 @Import(MizerServiceConfig.class)
 @ComponentScan({"org.nrg.dcm.edit.mizer", "org.nrg.dicom.dicomedit.mizer", "org.nrg.dicom.mizer.service.impl"})
 public class MizerServiceTestConfig {
-    @Bean
-    public DE6ScriptFactory de6ScriptFactory() {
-        return new DE6ScriptFactory();
-    }
-
-    @Bean
-    public ScriptApplicatorFactory scriptApplicatorFactory(DE6ScriptFactory scriptFactory) {
-        return new ScriptApplicatorFactory(scriptFactory);
-    }
 }
