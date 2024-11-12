131a46fcbeaf4c6ff7f6e200c854340378c9226f
Rick Herrick <rickherrick@flywheel.io>
2024-11-12 21:00:29 +0000

    Merged in fixes/xxx-289 (pull request #441)
    XXX-289
    
    * XXX-289
    
    
    Approved-by: James Ransford
    Approved-by: Bin Zhang

--- src/main/java/org/nrg/xapi/configuration/RestApiConfig.java
+++ src/main/java/org/nrg/xapi/configuration/RestApiConfig.java
@@ -26,13 +26,12 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 import java.util.Collections;
 import java.util.Locale;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @ComponentScan(value = {"org.nrg.xapi.model.users", "org.nrg.xapi.rest", "org.nrg.xnat.eventservice.rest", "org.nrg.xnat.snapshot.rest"}, includeFilters = @Filter(ControllerAdvice.class))
 @Import({SpawnerConfig.class})
 @Slf4j
