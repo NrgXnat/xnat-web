78c7d8ba1bd3ddd323089b2b43bfa41062c4ca5c
Rick Herrick <rick@rickherrick.com>
2024-11-14 16:37:44 +0000

    Merged in fixes/xxx-289 (pull request #442)
    Revert "Merged in fixes/xxx-289 (pull request #441)"
    
    * Revert "Merged in fixes/xxx-289 (pull request #441)"
    
    This reverts commit 131a46fcbeaf4c6ff7f6e200c854340378c9226f.
    
    
    Approved-by: Mohana Ramaratnam
    Approved-by: Bin Zhang

--- src/main/java/org/nrg/xapi/configuration/RestApiConfig.java
+++ src/main/java/org/nrg/xapi/configuration/RestApiConfig.java
@@ -26,12 +26,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import java.util.Collections;
 import java.util.Locale;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 @ComponentScan(value = {"org.nrg.xapi.model.users", "org.nrg.xapi.rest", "org.nrg.xnat.eventservice.rest", "org.nrg.xnat.snapshot.rest"}, includeFilters = @Filter(ControllerAdvice.class))
 @Import({SpawnerConfig.class})
 @Slf4j
