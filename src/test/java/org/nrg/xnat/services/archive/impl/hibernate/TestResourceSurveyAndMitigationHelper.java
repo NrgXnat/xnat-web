28c45f419bc67f2077fc4a7e607368711c269eb1
Bin Zhang <bin@xnatworks.io>
2025-09-05 16:22:55 -0500

    ResourceSurveyHelper

--- src/test/java/org/nrg/xnat/services/archive/impl/hibernate/TestResourceSurveyAndMitigationHelper.java
+++ src/test/java/org/nrg/xnat/services/archive/impl/hibernate/TestResourceSurveyAndMitigationHelper.java
@@ -5,7 +5,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.assertj.core.api.Condition;
-import org.dcm4che2.io.StopTagInputHandler;
+import org.dcm4che3.data.Tag;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -97,7 +97,7 @@ public class TestResourceSurveyAndMitigationHelper {
 
     private SerializerService   _serializer;
     private DicomFileNamer      _dicomFileNamer;
-    private StopTagInputHandler _stopTagInputHandler;
+    private int stopTag = Tag.PixelData;
 
     public TestResourceSurveyAndMitigationHelper() throws IOException {
         final Path tempDirectory = Files.createTempDirectory("TestResourceSurveyAndMitigationHelper-");
@@ -123,16 +123,16 @@ public class TestResourceSurveyAndMitigationHelper {
         _dicomFileNamer = dicomFileNamer;
     }
 
-    @Autowired
-    public void setStopTagInputHandler(final StopTagInputHandler stopTagInputHandler) {
-        _stopTagInputHandler = stopTagInputHandler;
+//    @Autowired
+    public void setStopTagInputHandler(int stopTag) {
+        this.stopTag = stopTag;
     }
 
     @Test
     @Ignore("Changes to workflow updates have made this difficult to get working, revisit later.")
     public void testSurveyReport() throws Exception {
         final ResourceSurveyRequest request      = getSurveyRequest();
-        final ResourceSurveyHelper  surveyHelper = new ResourceSurveyHelper(_mockEntityService, request, _serializer, _dicomFileNamer, _stopTagInputHandler);
+        final ResourceSurveyHelper  surveyHelper = new ResourceSurveyHelper(_mockEntityService, request, _serializer, _dicomFileNamer, stopTag);
         final ResourceSurveyReport  report       = surveyHelper.call();
 
         assertThat(report).isNotNull();
