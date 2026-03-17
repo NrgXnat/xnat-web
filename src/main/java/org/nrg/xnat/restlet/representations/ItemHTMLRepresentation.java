e25a9be28504bf5d20b667fa1d83d9bf27f94c5c
Timothy Olsen <tim@xnatworks.io>
2026-03-17 21:03:20 +0000

    XNAT-8670 Custom Report Identifier ignored in SecureResource
    Approved-by: Bin Zhang
    Approved-by: Kevin Archie

--- src/main/java/org/nrg/xnat/restlet/representations/ItemHTMLRepresentation.java
+++ src/main/java/org/nrg/xnat/restlet/representations/ItemHTMLRepresentation.java
@@ -13,10 +13,13 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.apache.turbine.util.TurbineException;
+import org.nrg.xdat.XDAT;
+import org.nrg.xdat.navigation.DefaultReportIdentifierI;
 import org.nrg.xdat.turbine.modules.actions.DisplayItemAction;
 import org.nrg.xdat.turbine.utils.TurbineUtils;
 import org.nrg.xft.XFTItem;
 import org.nrg.xft.exception.ElementNotFoundException;
+import org.nrg.xft.schema.design.SchemaElementI;
 import org.nrg.xft.security.UserI;
 import org.restlet.data.MediaType;
 import org.restlet.data.Request;
@@ -39,14 +42,14 @@ public class ItemHTMLRepresentation extends TurbineScreenRepresentation {
 		}
 		
 		
-		screen = DisplayItemAction.GetReportScreen(i.getItem().getGenericSchemaElement());
+		screen = resolveReportScreen(i.getItem().getGenericSchemaElement());
 	}
-	
+
 	public ItemHTMLRepresentation(XFTItem i,MediaType mt,Request request,UserI _user,String requested_screen,Map<String,Object> params) throws TurbineException,ElementNotFoundException {
 		super(mt,request,_user,params);
-		
+
 		TurbineUtils.setDataItem(data, i);
-		 
+
 		try {
 			if(i.getProperty("project")!=null){
 				data.getParameters().setString("project", i.getStringProperty("project"));
@@ -54,10 +57,10 @@ public class ItemHTMLRepresentation extends TurbineScreenRepresentation {
 		} catch (Throwable e1) {
 			logger.error("",e1);
 		}
-		
-		
+
+
 		if(requested_screen==null){
-			screen = DisplayItemAction.GetReportScreen(i.getItem().getGenericSchemaElement());
+			screen = resolveReportScreen(i.getItem().getGenericSchemaElement());
 		}else{
 			if(!requested_screen.endsWith(".vm"))requested_screen+=".vm";
 			screen = requested_screen;
@@ -68,5 +71,21 @@ public class ItemHTMLRepresentation extends TurbineScreenRepresentation {
 	public String getScreen() {
 		return screen;
 	}
-	
+
+	/**
+	 * Resolves the report screen using the configured UI.defaultReportIdentifier,
+	 * falling back to the static DisplayItemAction.GetReportScreen() method.
+	 */
+	private String resolveReportScreen(SchemaElementI schemaElement) {
+		final String defaultReportIdentifierClass = XDAT.getSiteConfigurationProperty("UI.defaultReportIdentifier", "org.nrg.xdat.navigation.DefaultReportIdentifier");
+		try {
+			final Object object = Class.forName(defaultReportIdentifierClass).getDeclaredConstructor().newInstance();
+			if (object instanceof DefaultReportIdentifierI) {
+				return ((DefaultReportIdentifierI) object).identifyReport(data, null);
+			}
+		} catch (Throwable e) {
+			logger.error("Failed to use configured report identifier " + defaultReportIdentifierClass + ", falling back to default", e);
+		}
+		return DisplayItemAction.GetReportScreen(schemaElement);
+	}
 }
